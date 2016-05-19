c2e6a24dce2772bdbc4919a5961f5af65d6fe9e4
ilyailya <bkmz2@yandex.ru>
2016-05-19 23:42:00 +0300

    Initial Commit

---
+++ Seeda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seeda
+//
+//  Created by Илья Железников on 19/05/16.
+//  Copyright © 2016 Илья Железников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
